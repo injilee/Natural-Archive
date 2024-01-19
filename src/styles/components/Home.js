e8ac92d07fb0f4c47127d9f4a3255f7da377320c
Lee Inji <lij8016@gmail.com>
2024-01-19 13:51:36 +0900

    💄 Update Home, Navbar style

--- src/styles/components/Home.js
+++ src/styles/components/Home.js
@@ -1,67 +1,77 @@
 import styled from 'styled-components';
 
 export const MainWrap = styled.div`
-   display: flex;
-   justify-content: space-around;
-   background-color: black;
-   min-height: 100vh;
-   align-items: center;
+  display: flex;
+  justify-content: space-around;
+  background-color: black;
+  min-height: 100vh;
+  align-items: center;
 `;
 
 export const MainImg = styled.ul`
-   position: relative;
-   display: flex;
-   flex-direction: column;
+  position: relative;
+  display: flex;
+  flex-direction: column;
 
-   li {
-      width: 100%;
-      margin: 0;
+  li {
+    width: 100%;
+    margin: 0;
 
-      a {
-         position: relative;
-         color: white;
-         font-size: 65px;
-         font-family: 'Playfair Display', serif;
-         margin-left: 15px;
-         &::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            height: 2px;
-            background-color: white;
-            transform: scaleX(0);
-            transform-origin: bottom right;
-            transition: transform 0.25s ease-out;
-         }
+    a {
+      position: relative;
+      color: white;
+      font-size: 2.8125rem;
+      font-family: 'Playfair Display', serif;
+      margin-left: 15px;
+      transition-property: font-size;
+      transition-duration: 0.3s;
+      transition-timing-function: ease;
 
-         &:hover {
-            color: orange;
-            &::after {
-               transform: scaleX(1);
-               transform-origin: bottom left;
-            }
-         }
+      @media screen and (min-width: 768px) {
+        & {
+          font-size: 4.0625rem;
+        }
       }
-   }
+
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: white;
+        transform: scaleX(0);
+        transform-origin: bottom right;
+        transition: transform 0.25s ease-out;
+      }
+
+      &:hover {
+        color: orange;
+        &::after {
+          transform: scaleX(1);
+          transform-origin: bottom left;
+        }
+      }
+    }
+  }
 `;
 
 export const SideImage = styled.div`
-   display: flex;
-   align-items: center;
-   width: 40%;
+  display: flex;
+  align-items: center;
+  width: 40%;
 
-   img {
-      display: none;
-   }
+  img {
+    display: none;
+  }
 
-   @media screen and (min-width: 768px) {
-      img {
-         display: block;
-         width: 100%;
-         height: 590px;
-         object-fit: contain;
-      }
-   }
+  @media screen and (min-width: 768px) {
+    img {
+      display: block;
+      width: 100%;
+      height: 590px;
+      object-fit: contain;
+    }
+  }
 `;
--- src/styles/components/NavBar.js
+++ src/styles/components/NavBar.js
@@ -1,75 +1,81 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-   height: 100px;
-   background-color: black;
+  height: 100px;
+  background-color: black;
 `;
 
 export const Nav = styled.nav`
-   display: flex;
-   flex-direction: row;
-   justify-content: space-between;
-   align-items: center;
-   width: 90%;
-   height: 100px;
-   margin: auto;
-   color: white;
-   z-index: 10;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 10;
+  width: 90%;
+  height: 100px;
+  color: white;
+  margin: auto;
 
-   h1 {
-      width: 230px;
-      height: auto;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-content: center;
+  h1 {
+    width: 230px;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
 
-      span {
-         color: white;
-         font-size: 30px;
-         font-weight: 500;
-         font-family: 'Ubuntu', sans-serif;
-         &:hover {
-            text-decoration: none;
-         }
-      }
-   }
-
-   span {
-      cursor: pointer;
+    span {
+      color: white;
+      font-size: 30px;
+      font-weight: 500;
+      font-family: 'Ubuntu', sans-serif;
       &:hover {
-         text-decoration: underline;
+        text-decoration: none;
       }
-   }
+    }
+  }
+
+  span {
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 export const SlideMenu = styled.div`
-   position: sticky;
-   top: 0;
-   left: 0;
-   display: block;
-   width: 100%;
-   padding: 30px 0;
-   background-color: black;
-   z-index: 10;
-   opacity: ${({ open }) => (open ? '1' : '0')};
-   visibility: ${({ open }) => (open ? 'visibility' : 'hidden')};
-   transition: opacity 0.2s, visibility 0.1s;
-   
-   ul {
-      width: 90%;
-      margin: 0 auto;
-   }
+  position: sticky;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  padding: 30px 0;
+  text-align: center;
+  background-color: black;
+  z-index: 10;
+  opacity: ${({ open }) => (open ? '1' : '0')};
+  visibility: ${({ open }) => (open ? 'visibility' : 'hidden')};
+  transition: opacity 0.2s, visibility 0.1s;
 
-   li {
-      font-size: 30px;
-      margin: 25px 0;
-      
-      a{
-         display:block;
-         color: white;
-         &:hover {
-            text-decoration: underline;
+  ul {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  li {
+    font-size: 30px;
+    margin: 25px 0;
+
+    a {
+      display: block;
+      color: white;
+      &:hover {
+        color: black;
       }
-   }
+    }
+
+    &:hover {
+      background-color: lemonchiffon;
+    }
+  }
 `;
