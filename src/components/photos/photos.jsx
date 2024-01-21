2cdd2bcaac3ba6fbb3934eb510e92723eba417d6
Lee Inji <lij8016@gmail.com>
2024-01-21 22:40:24 +0900

    💄 Apply grid system style photos component

--- src/components/photos/photos.jsx
+++ src/components/photos/photos.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
-import { Photograpy } from '../../styles/components/photos';
+import { Photograpy, PhotoContent, PhotoList } from '../../styles/components/photos';
+import data from '../service/data.json';
 
 const Photos = () => {
-   return (
-      <Photograpy>
-         <p>Coming Soon</p>
-         <span>곧 업데이트 될 예정입니다 :)</span>
-      </Photograpy>
-   );
+  // 좌클릭 이벤트 발생시 모달창 보이기
+  const handleOnClick = e => {};
+
+  //   우클릭 이벤트가 발생했을때, 우클릭 이벤트 방지
+  const handleRightClick = e => {
+    e.preventDefault();
+    alert('이미지의 우클릭은 방지됩니다.');
+  };
+
+  return (
+    <Photograpy>
+      <PhotoContent>
+        {data.archivePictures[5].main.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+        {data.archivePictures[4].project.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+        {data.archivePictures[3].travel.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+        {data.archivePictures[2].life.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+        {data.archivePictures[1].landscape.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+        {data.archivePictures[0].spaces.map(photo => (
+          <PhotoList key={photo.key} id={photo.key}>
+            <div>
+              <img onClick={handleOnClick} onContextMenu={handleRightClick} src={photo.imgUrl} alt={photo.alt} />
+            </div>
+          </PhotoList>
+        ))}
+      </PhotoContent>
+    </Photograpy>
+  );
 };
 export default Photos;
--- src/styles/components/photos.js
+++ src/styles/components/photos.js
@@ -1,23 +1,72 @@
 import styled from 'styled-components';
 
 export const Photograpy = styled.section`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   widht: 100%;
-   height: 100vh;
-   background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 50px 15px;
+  background-color: white;
+`;
 
-   p {
-      color: white;
-      font-size: 70px;
-      font-family: 'Secular One', sans-serif;
-   }
+export const PhotoContent = styled.ul`
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: 1fr;
+  grid-auto-rows: auto;
+  grid-gap: 0.3rem;
 
-   span {
-      color: white;
-      font-size: 30px;
-      font-family: 'Nanum Myeongjo', serif;
-   }
+  @media screen and (min-width: 480px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 1200px;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 0.5rem;
+  }
+`;
+
+export const PhotoList = styled.li`
+  display: flex;
+  grid-column: ${({ id }) => (id === 'postBox' ? '2 / 2' : 'auto')};
+  grid-row: ${({ id }) => (id === 'postBox' ? '3 / 6' : 'auto')};
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media screen and (min-width: 768px) {
+    grid-column: ${({ id }) => {
+      switch (id) {
+        case 'postBox':
+          return '2 / 2';
+        case 'life1':
+          return '2 / 4';
+        case 'spaces5':
+          return '1 / 3';
+        default:
+          return 'auto';
+      }
+    }};
+
+    grid-row: ${({ id }) => {
+      switch (id) {
+        case 'postBox':
+          return '3 / 6';
+        case 'life1':
+          return '8 / 10';
+        case 'spaces5':
+          return '18 / 19';
+        default:
+          return 'auto';
+      }
+    }};
+  }
 `;
